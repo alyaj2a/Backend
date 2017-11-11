50dcf9b78a1b47aec9dcbd5e903eeffdf5c34262
Aly Machaca <alyaj2a@gmail.com>
2017-11-10 19:15:16 -0500

    Added class for work with Url and Link

--- modules/forcontu_pages/src/Controller/ForcontuPagesController.php
+++ modules/forcontu_pages/src/Controller/ForcontuPagesController.php
@@ -7,6 +7,8 @@ namespace Drupal\forcontu_pages\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Drupal\user\UserInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 class ForcontuPagesController extends ControllerBase{
 
@@ -72,6 +74,57 @@ class ForcontuPagesController extends ControllerBase{
       '#theme' => 'item_list',
       '#items' => $list,
       '#title' => $this->t('User data:'),
+    );
+    return $output;
+  }
+  public function links(){
+
+    $url1 = Url::fromRoute('block.admin_display');
+    $link1 = Link::fromTextAndUrl(t('Go to the Block administration page'), $url1);
+    $list[] = $link1;
+    $list[] = $this->t('This text contains a link to %enlace. Just convert it to String to use it into a text.',
+              ['%enlace' => $link1->toString()]);
+
+    $url3 = Url::fromRoute('<front>');
+    $link3 = Link::fromTextAndUrl(t('Go to Front page'), $url3);
+    $list[] = $link3;
+
+    $url4 = Url::fromRoute('entity.node.canonical', ['node' => 1]);
+    $link4 = Link::fromTextAndUrl(t('Link to node/1'), $url4);
+    $list[] = $link4;
+
+    $url5 = Url::fromRoute('entity.node.edit_form', ['node' => 1]);
+    $link5 = Link::fromTextAndUrl(t('Link to edit node/1'), $url5);
+    $list[] = $link5;
+
+    $url6 = Url::fromUri('https://www.forcontu.com');
+    $link6 = Link::fromTextAndUrl(t('Link to www.forcontu.com'), $url6);
+    $list[] = $link6;
+
+    $url7 = Url::fromUri('internal:/core/themes/bartik/css/layout.css');
+    $link7 = Link::fromTextAndUrl(t('Link to layout.css'), $url7);
+    $list[] = $link7;
+
+    $url8 = Url::fromUri('https://www.drupal.org');
+    $link_options = array(
+      'attributes' => array(
+        'class' => array(
+          'external-link',
+          'list'
+        ),
+        'target' => '_blank',
+        'title' => 'Go to drupal.org',
+      ),
+    );
+    $url8->setOptions($link_options);
+    $link8 = Link::fromTextAndUrl(t('Link to drupal.org'), $url8);
+    $list[] = $link8;
+
+
+    $output['forcontu_pages_links'] = array(
+      '#theme' => 'item_list',
+      '#items' => $list,
+      '#title' => $this->t('Examples of links'),
     );
     return $output;
   }
